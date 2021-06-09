214363c323b6075ccf13390bfe431983e982e163
carlosbandelliv@gmail.com <carlosbandelliv@gmail.com>
2021-06-08 22:53:20 -0300

    O que é VUE.JS

---
+++ VUE.TXT.js
@@ -0,0 +1,14 @@
+ele é uma framework de reatividade
+estilo react.js e angular.js 
+
+O grande foco do VUE.JS é simpleficar seu trabalho com javascript
+
+
+O foco do VUE.JS é criação de aplicações de SPA
+SPA = Simple Page Apliaction
+
+o que seria uma aplicação de pagina unica?
+
+Uma aplicação de pagina unica é uma aplicação WEB estilo discord, para eu passar de uma pagina para outro eu não preciso
+recarregar a tela novamente.
+
